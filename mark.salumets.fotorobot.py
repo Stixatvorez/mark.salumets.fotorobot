59b14cb07213ff2af22f18d4d3fb6dcd785afe7a
opilane <opilane@HP-CZC6297B8Z>
2022-01-14 10:18:27 +0200

    fotoboberrrr

--- mark.salumets.fotorobot.py
+++ mark.salumets.fotorobot.py
@@ -11,10 +11,17 @@ def head():
 		a=1
 	elif a==1:
 		draw.create_rectangle((15, 15, 290, 290), fill="white", outline="black",width="2")
+def nose():
+	draw.create_polygon([(150, 163), (181, 163), (150,130 )],fill="grey")
+
+def mouth():
+	draw.create_line((213, 215, 90, 215),fill="black",width=3)
+
 def eyes():
-	global b,draw
-	if b==0:
-		draw.create_oval((125,100,175,100))
+	draw.create_rectangle((72,72,130,130),fill="white")
+	draw.create_oval((165,72,221,130),fill="white")
+	draw.create_rectangle((82,82,117,120),fill="black")
+	draw.create_oval((175,82,211,120),fill="black")
 
 tk=Tk()
 tk.title("ФОТОБОБЕР")
@@ -28,9 +35,9 @@ f2.pack(side=RIGHT)
 name=Label(f1,text="Создание фотобобра",font="Calibri 26",fg="black",bg="lightblue",justify=CENTER)
 name.pack(side=TOP)
 head=Checkbutton(f1,text="golova",font="Calibri 26",fg="black",bg="lightblue",relief=GROOVE,command=head)
-eyes=Checkbutton(f1,text="glaza",font="Calibri 26",fg="black",bg="lightblue",relief=GROOVE)
-nose=Checkbutton(f1,text="nos",font="Calibri 26",fg="black",bg="lightblue",relief=GROOVE)
-mouth=Checkbutton(f1,text="rotik",font="Calibri 26",fg="black",bg="lightblue",relief=GROOVE)
+eyes=Checkbutton(f1,text="glaza",font="Calibri 26",fg="black",bg="lightblue",relief=GROOVE,command=eyes)
+nose=Checkbutton(f1,text="nos",font="Calibri 26",fg="black",bg="lightblue",relief=GROOVE,command=nose)
+mouth=Checkbutton(f1,text="rotik",font="Calibri 26",fg="black",bg="lightblue",relief=GROOVE,command=mouth)
 
 head.pack()
 eyes.pack()
